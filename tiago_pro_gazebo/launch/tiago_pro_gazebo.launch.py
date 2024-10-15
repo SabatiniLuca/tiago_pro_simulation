cc3c576f08269109bdc400ec6b4838461c2898cd
martinaannicelli <martina.annicelli@pal-robotics.com>
2024-10-15 16:12:12 +0200

    added advanced nav to navigation

--- tiago_pro_gazebo/launch/tiago_pro_gazebo.launch.py
+++ tiago_pro_gazebo/launch/tiago_pro_gazebo.launch.py
@@ -99,6 +99,7 @@ def declare_actions(launch_description: LaunchDescription, launch_args: LaunchAr
             'use_sim_time': LaunchConfiguration('use_sim_time'),
             'world_name': launch_args.world_name,
             'slam': launch_args.slam,
+            'advanced_navigation': launch_args.advanced_navigation
         },
         condition=IfCondition(LaunchConfiguration("navigation")))
 
